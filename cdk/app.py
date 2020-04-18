65f82610d9d6e68d930cd6006e07372d1f106b52
Dylan Vaughn <dylan@ordinaryexperts.com>
2020-04-18 10:16:36 -0700

    moving cdk into its old folder

---
+++ cdk/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from drupal.drupal_stack import DrupalStack
+
+env_oe_prod_us_west_1 = core.Environment(account="440643590597", region="us-west-1")
+
+app = core.App()
+DrupalStack(app, "drupal", env=env_oe_prod_us_west_1)
+
+app.synth()
