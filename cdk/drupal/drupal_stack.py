c73fc2cc5ba221ba5362d9b469f87a91fefcc4c2
Dylan Vaughn <dylan@ordinaryexperts.com>
2020-04-19 09:38:09 -0700

    start on sns

--- cdk/drupal/drupal_stack.py
+++ cdk/drupal/drupal_stack.py
@@ -1,6 +1,8 @@
+import json
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_rds as rds
 from aws_cdk import aws_secretsmanager as secretsmanager
+from aws_cdk import aws_sns as sns
 from aws_cdk import core
 
 class DrupalStack(core.Stack):
@@ -15,7 +17,11 @@ class DrupalStack(core.Stack):
         )
         secret = secretsmanager.Secret(
             self,
-            "secret"
+            "secret",
+            generate_secret_string=secretsmanager.SecretStringGenerator(
+                secret_string_template=json.dumps({"username":"user"}),
+                generate_string_key="password"
+            )
         )
         db_subnet_group = rds.CfnDBSubnetGroup(
             self,
@@ -23,10 +29,45 @@ class DrupalStack(core.Stack):
             db_subnet_group_description="test",
             subnet_ids=vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE).subnet_ids
         )
-        # rds = ec2.Rds(
-        #     self,
-        #     "rds"
-        # )
+        db_cluster_parameter_group = rds.CfnDBClusterParameterGroup(
+            self,
+            "DBClusterParameterGroup",
+            description="test",
+            family="aurora5.6",
+            parameters={
+                "character_set_client": "utf8",
+                "character_set_connection": "utf8",
+                "character_set_database": "utf8",
+                "character_set_filesystem": "utf8",
+                "character_set_results": "utf8",
+                "character_set_server": "utf8",
+                "collation_connection": "utf8_general_ci",
+                "collation_server": "utf8_general_ci"
+            }
+        )
+        db_cluster = rds.CfnDBCluster(
+            self,
+            "DBCluster",
+            engine="aurora",
+            db_cluster_parameter_group_name=db_cluster_parameter_group.ref,
+            db_subnet_group_name=db_subnet_group.ref,
+            engine_mode="serverless",
+            master_username="dbadmin",
+            # TODO: get this working
+            # master_user_password=core.SecretValue.cfnDynamicReference(secret),
+            master_user_password="dbpassword",
+            scaling_configuration={
+                "auto_pause": True,
+                "min_capacity": 1,
+                "max_capacity": 2,
+                "seconds_until_auto_pause": 30
+            },
+            storage_encrypted=True
+        )
+        notification_topic = sns.Topic(
+            self,
+            "NotificationTopic"
+        )
         # alb = alb(
         #     self,
         #     "alb"
--- cdk/setup.py
+++ cdk/setup.py
@@ -5,6 +5,8 @@ with open("README.md") as fp:
     long_description = fp.read()
 
 
+CDK_VERSION="1.33.1"
+
 setuptools.setup(
     name="drupal",
     version="0.0.1",
@@ -19,10 +21,11 @@ setuptools.setup(
     packages=setuptools.find_packages(where="drupal"),
 
     install_requires=[
-        "aws-cdk.core==1.33.0",
-        "aws-cdk.aws-ec2==1.33.0",
-        "aws-cdk.aws-rds==1.33.0",
-        "aws-cdk.aws-secretsmanager==1.33.0"
+        f"aws-cdk.core=={CDK_VERSION}",
+        f"aws-cdk.aws-ec2=={CDK_VERSION}",
+        f"aws-cdk.aws-rds=={CDK_VERSION}",
+        f"aws-cdk.aws-secretsmanager=={CDK_VERSION}",
+        f"aws-cdk.aws-sns=={CDK_VERSION}"
     ],
 
     python_requires=">=3.6",
