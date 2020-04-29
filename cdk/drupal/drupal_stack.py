8431fc4d0ce7e18eabad21dd7e3f41d1460d6fb9
Aaron Carlton <aaron.r.carlton@gmail.com>
2020-04-29 14:47:02 +0200

    moving app launch config user data to read from file; read policy from oe-patterns-dev githubartifactbucket; temporary code dl for deployment; new packer image with libapache-mod-php7.2 and tweaks for drupal vhost

--- cdk/drupal/drupal_stack.py
+++ cdk/drupal/drupal_stack.py
@@ -4,7 +4,7 @@ from aws_cdk import (
     aws_logs, aws_rds, aws_secretsmanager, aws_sns, core
 )
 
-AMI="ami-045479e70f8eb387b"
+AMI="ami-0423ff58236b36794"
 TWO_YEARS_IN_DAYS=731
 
 class DrupalStack(core.Stack):
@@ -212,6 +212,17 @@ class DrupalStack(core.Stack):
                             resources=['*']
                         )
                     ]
+                ),
+                "AllowGetFromGithubArtifactBucket": aws_iam.PolicyDocument(
+                    statements=[
+                        aws_iam.PolicyStatement(
+                            effect=aws_iam.Effect.ALLOW,
+                            actions=[
+                                's3:GetObject',
+                            ],
+                            resources=['arn:aws:s3:::github-user-and-bucket-githubartifactbucket-1c9jk3sjkqv8p/*']
+                        )
+                    ]
                 )
             }
         )
@@ -226,6 +237,8 @@ class DrupalStack(core.Stack):
             "AppInstanceProfile",
             roles=[app_instance_role.role_name]
         )
+        with open('drupal/scripts/app_launch_config_user_data.sh') as f:
+            app_launch_config_user_data = f.read()
         launch_config = aws_autoscaling.CfnLaunchConfiguration(
             self,
             "AppLaunchConfig",
@@ -235,135 +248,7 @@ class DrupalStack(core.Stack):
             security_groups=[sg.security_group_id],
             user_data=(
                 core.Fn.base64(
-                    core.Fn.sub(
-                        """#!/bin/bash
-cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-{
-  "agent": {
-    "metrics_collection_interval": 60,
-    "run_as_user": "root",
-    "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
-  },
-  "metrics": {
-    "metrics_collected": {
-      "collectd": {
-        "metrics_aggregation_interval": 60
-      },
-      "disk": {
-        "measurement": ["used_percent"],
-        "metrics_collection_interval": 60,
-        "resources": ["*"]
-      },
-      "mem": {
-        "measurement": ["mem_used_percent"],
-        "metrics_collection_interval": 60
-      }
-    },
-    "append_dimensions": {
-      "ImageId": "\${!aws:ImageId}",
-      "InstanceId": "\${!aws:InstanceId}",
-      "InstanceType": "\${!aws:InstanceType}",
-      "AutoScalingGroupName": "\${!aws:AutoScalingGroupName}"
-    }
-  },
-  "logs": {
-    "logs_collected": {
-      "files": {
-        "collect_list": [
-          {
-            "file_path": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/dpkg.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/dpkg.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/apt/history.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/apt/history.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/cloud-init.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/cloud-init.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/cloud-init-output.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/cloud-init-output.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/auth.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/auth.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/syslog",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/syslog",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/amazon/ssm/amazon-ssm-agent.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/amazon/ssm/amazon-ssm-agent.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/amazon/ssm/errors.log",
-            "log_group_name": "${DrupalSystemLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/amazon/ssm/errors.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/apache2/access.log",
-            "log_group_name": "${DrupalAccessLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/apache2/access.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/apache2/error.log",
-            "log_group_name": "${DrupalErrorLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/apache2/error.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/apache2/access-ssl.log",
-            "log_group_name": "${DrupalAccessLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/apache2/access-ssl.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/apache2/error-ssl.log",
-            "log_group_name": "${DrupalErrorLogGroup}",
-            "log_stream_name": "{instance_id}-/var/log/apache2/error-ssl.log",
-            "timezone": "UTC"
-          }
-        ]
-      }
-    },
-    "log_stream_name": "{instance_id}"
-  }
-}
-EOF
-systemctl enable amazon-cloudwatch-agent
-systemctl start amazon-cloudwatch-agent
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
-  -keyout /etc/ssl/private/apache-selfsigned.key \
-  -out /etc/ssl/certs/apache-selfsigned.crt \
-  -subj '/CN=localhost'
-systemctl enable apache2 && systemctl start apache2
-"""
-                    )
+                    core.Fn.sub(app_launch_config_user_data)
                 )
             )
         )
---
+++ cdk/drupal/scripts/app_launch_config_user_data.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# aws cloudwatch
+cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+{
+  "agent": {
+    "metrics_collection_interval": 60,
+    "run_as_user": "root",
+    "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
+  },
+  "metrics": {
+    "metrics_collected": {
+      "collectd": {
+        "metrics_aggregation_interval": 60
+      },
+      "disk": {
+        "measurement": ["used_percent"],
+        "metrics_collection_interval": 60,
+        "resources": ["*"]
+      },
+      "mem": {
+        "measurement": ["mem_used_percent"],
+        "metrics_collection_interval": 60
+      }
+    },
+    "append_dimensions": {
+      "ImageId": "\${!aws:ImageId}",
+      "InstanceId": "\${!aws:InstanceId}",
+      "InstanceType": "\${!aws:InstanceType}",
+      "AutoScalingGroupName": "\${!aws:AutoScalingGroupName}"
+    }
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/dpkg.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/dpkg.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/apt/history.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/apt/history.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/cloud-init.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/cloud-init.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/cloud-init-output.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/cloud-init-output.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/auth.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/auth.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/syslog",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/syslog",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/amazon/ssm/amazon-ssm-agent.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/amazon/ssm/amazon-ssm-agent.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/amazon/ssm/errors.log",
+            "log_group_name": "${DrupalSystemLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/amazon/ssm/errors.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/apache2/access.log",
+            "log_group_name": "${DrupalAccessLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/apache2/access.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/apache2/error.log",
+            "log_group_name": "${DrupalErrorLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/apache2/error.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/apache2/access-ssl.log",
+            "log_group_name": "${DrupalAccessLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/apache2/access-ssl.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/apache2/error-ssl.log",
+            "log_group_name": "${DrupalErrorLogGroup}",
+            "log_stream_name": "{instance_id}-/var/log/apache2/error-ssl.log",
+            "timezone": "UTC"
+          }
+        ]
+      }
+    },
+    "log_stream_name": "{instance_id}"
+  }
+}
+EOF
+systemctl enable amazon-cloudwatch-agent
+systemctl start amazon-cloudwatch-agent
+
+# Drupal; TODO: remove after CodePipline integration
+aws s3 cp s3://github-user-and-bucket-githubartifactbucket-1c9jk3sjkqv8p/aws-marketplace-oe-patterns-drupal-example-site/refs/heads/develop.tar.gz .
+tar xvfz develop.tar.gz
+mv -T drupal /var/www/drupal
+
+# apache
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+  -keyout /etc/ssl/private/apache-selfsigned.key \
+  -out /etc/ssl/certs/apache-selfsigned.crt \
+  -subj '/CN=localhost'
+systemctl enable apache2 && systemctl start apache2
+
