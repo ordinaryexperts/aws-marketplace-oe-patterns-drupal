bcc8103fdfaa4bc1d07d41cabf4a40580f144689
Dylan Vaughn <dylan@ordinaryexperts.com>
2020-05-01 00:07:35 -0700

    tweak to setup

--- setup-env.sh
+++ setup-env.sh
@@ -4,8 +4,10 @@ export CDK_VERSION=1.36.1
 export PACKER_VERSION=1.5.5
 export TASKCAT_VERSION=0.9.17
 
+# system upgrades and tools
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update && apt-get -y upgrade
+apt-get -y install unzip vim wget
 
 # taskcat
 apt-get -y install python3 python3-pip
@@ -16,7 +18,6 @@ apt-get -y install npm
 npm install -g aws-cdk@$CDK_VERSION
 
 # packer
-apt-get -y install unzip wget
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/$PACKER_VERSION/packer_${PACKER_VERSION}_linux_amd64.zip
 unzip /tmp/packer.zip -d /usr/local/bin/
 rm /tmp/packer.zip
