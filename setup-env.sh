a7b1b82e3b6cc3e114f60e7addda522466fef153
Dylan Vaughn <dylan@ordinaryexperts.com>
2020-05-10 23:50:55 -0700

    quiet mode

--- setup-env.sh
+++ setup-env.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
+echo "$(date): Starting setup-env.sh"
+
 export CDK_VERSION=1.36.1
 export PACKER_VERSION=1.5.5
 export TASKCAT_VERSION=0.9.17
 
 # system upgrades and tools
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y update && apt-get -y upgrade
-apt-get -y install curl unzip vim wget
+apt-get -y -qq update && apt-get -y -qq upgrade
+apt-get -y -qq install curl unzip vim wget
 
 # aws cli
 cd /tmp
-curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
+curl --silent --show-error https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
 unzip awscliv2.zip
 ./aws/install
 cd -
 
 # taskcat
-apt-get -y install python3 python3-pip
+apt-get -y -qq install python3 python3-pip
 pip3 install taskcat==$TASKCAT_VERSION
 
 # cdk
-apt-get -y install npm
+apt-get -y -qq install npm
 npm install -g aws-cdk@$CDK_VERSION
 
 # packer
-wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/$PACKER_VERSION/packer_${PACKER_VERSION}_linux_amd64.zip
+wget -q -O /tmp/packer.zip https://releases.hashicorp.com/packer/$PACKER_VERSION/packer_${PACKER_VERSION}_linux_amd64.zip
 unzip /tmp/packer.zip -d /usr/local/bin/
 rm /tmp/packer.zip
+
+echo "$(date): Finished setup-env.sh"
