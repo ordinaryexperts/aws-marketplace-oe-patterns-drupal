d171493f94676e4fddce85cebc861f21bdd3fe84
Dylan Vaughn <dylan@ordinaryexperts.com>
2020-05-14 21:04:25 -0700

    changing docroot

--- cdk/drupal/drupal_stack.py
+++ cdk/drupal/drupal_stack.py
@@ -20,7 +20,7 @@ from aws_cdk import (
     core
 )
 
-AMI="ami-0a863e107cefed1f4"
+AMI="ami-02d7b8edeecb86c6f"
 DB_SNAPSHOT="arn:aws:rds:us-east-1:992593896645:cluster-snapshot:oe-patterns-drupal-default-20200504"
 TWO_YEARS_IN_DAYS=731
 
